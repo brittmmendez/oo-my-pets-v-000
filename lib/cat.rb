7655d0afe6aafa34b3496c8b290b1c1e1c15c0ea
Brittany Morris <brittmmendez@gmail.com>
2017-12-18 15:54:22 +0000

    automatically backed up by learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -5,6 +5,6 @@ class Cat
   def initialize(name)
     @name=name
     @mood="nervous"
-    @species="Cat"
+  
   end
 end
--- lib/owner.rb
+++ lib/owner.rb
@@ -48,7 +48,7 @@ class Owner
   end
 
   def sell_pets
-    @pets={fishes: [], cats: [], dogs: []}    
+    @pets={fishes: [], cats: [], dogs: []}
   end
 
 
