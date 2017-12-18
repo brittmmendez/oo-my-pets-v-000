8ac67203356ba41ce2db2dbeafc706b04e662c8d
Brittany Morris <brittmmendez@gmail.com>
2017-12-18 15:53:04 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -43,9 +43,10 @@ class Owner
     @pets[:dogs]<<dog
   end
 
-  def walk_dogs
-    Dog.mood="Happy"
+  def list_pets
+    "I have #{@pets[:fishes].length} fish, #{@pets[:dogs].length} dog(s), and #{@pets[:cats].length} cat(s)."
   end
 
+  
 
 end
